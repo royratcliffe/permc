3205c6b506d457822f7850694a1d65fce2da93e8
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-18 20:10:37 +0000

    Ignore command-line arguments

--- tests/perm_test.c
+++ tests/perm_test.c
@@ -2,7 +2,16 @@
 
 #include <stdlib.h>
 
+/*!
+ * \brief Test the next_permutation_ints function.
+ * \param argc The number of command-line arguments.
+ * \param argv The command-line arguments.
+ * \return EXIT_SUCCESS if all tests pass, EXIT_FAILURE otherwise.
+ */
 int perm_test(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+
   int ints1[] = {1, 2, 3};
   const int length1 = sizeof(ints1) / sizeof(ints1[0]);
   if (!next_permutation_ints(ints1, length1) || ints1[0] != 1 || ints1[1] != 3 || ints1[2] != 2) {
