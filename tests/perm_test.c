5fa21a07e0de58ce0101d9356e7625ff633e6f81
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-18 20:04:44 +0000

    Add permutation tests for next_permutation_ints function

--- tests/perm_test.c
+++ tests/perm_test.c
@@ -2,4 +2,26 @@
 
 #include <stdlib.h>
 
-int perm_test(int argc, char **argv) { return EXIT_SUCCESS; }
+int perm_test(int argc, char **argv) {
+  int ints1[] = {1, 2, 3};
+  const int length1 = sizeof(ints1) / sizeof(ints1[0]);
+  if (!next_permutation_ints(ints1, length1) || ints1[0] != 1 || ints1[1] != 3 || ints1[2] != 2) {
+    return EXIT_FAILURE;
+  }
+  if (!next_permutation_ints(ints1, length1) || ints1[0] != 2 || ints1[1] != 1 || ints1[2] != 3) {
+    return EXIT_FAILURE;
+  }
+  if (!next_permutation_ints(ints1, length1) || ints1[0] != 2 || ints1[1] != 3 || ints1[2] != 1) {
+    return EXIT_FAILURE;
+  }
+  if (!next_permutation_ints(ints1, length1) || ints1[0] != 3 || ints1[1] != 1 || ints1[2] != 2) {
+    return EXIT_FAILURE;
+  }
+  if (!next_permutation_ints(ints1, length1) || ints1[0] != 3 || ints1[1] != 2 || ints1[2] != 1) {
+    return EXIT_FAILURE;
+  }
+  if (next_permutation_ints(ints1, length1) || ints1[0] != 1 || ints1[1] != 2 || ints1[2] != 3) {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
