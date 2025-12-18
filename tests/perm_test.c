1a88531a067b4a143212003753b210034d5d7dca
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-18 20:24:12 +0000

    Add detailed comments to permutation test cases in perm_test.c

--- tests/perm_test.c
+++ tests/perm_test.c
@@ -14,6 +14,18 @@ int perm_test(int argc, char **argv) {
 
   int ints1[] = {1, 2, 3};
   const int length1 = sizeof(ints1) / sizeof(ints1[0]);
+  /*
+   * Test all permutations of three elements.
+   * There are 3! = 6 permutations.
+   *
+   * They are:
+   * 1 2 3
+   * 1 3 2
+   * 2 1 3
+   * 2 3 1
+   * 3 1 2
+   * 3 2 1
+   */
   if (!next_permutation_of_ints(ints1, length1) || ints1[0] != 1 || ints1[1] != 3 || ints1[2] != 2) {
     return EXIT_FAILURE;
   }
@@ -29,6 +41,10 @@ int perm_test(int argc, char **argv) {
   if (!next_permutation_of_ints(ints1, length1) || ints1[0] != 3 || ints1[1] != 2 || ints1[2] != 1) {
     return EXIT_FAILURE;
   }
+  /*
+   * Now at the last permutation. The next call should reset to the first
+   * permutation and return false.
+   */
   if (next_permutation_of_ints(ints1, length1) || ints1[0] != 1 || ints1[1] != 2 || ints1[2] != 3) {
     return EXIT_FAILURE;
   }
